6750b5754893ace6b28c25f435522b7b595186a7
nyzhhd <3268407065@qq.com>
2022-10-04 17:16:48 +0800

    122

--- learning_node/learning_node/node_helloworld_class.py
+++ learning_node/learning_node/node_helloworld_class.py
@@ -19,7 +19,7 @@ class HelloWorldNode(Node):
         super().__init__(name)                       # ROS2节点父类初始化
         while rclpy.ok():                            # ROS2系统是否正常运行
             self.get_logger().info("Hello World")    # ROS2日志输出
-            time.sleep(10)                          # 休眠控制循环时间
+            time.sleep(5)                          # 休眠控制循环时间
 
 def main(args=None):                                 # ROS2节点主入口main函数
     rclpy.init(args=args)                            # ROS2 Python接口初始化
